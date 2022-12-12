7893933049097ec8c0756f2868518a975f1faa4d
Tales Gomes <tales.santos.gomes@outlook.com>
2022-12-12 10:02:37 -0300

    finalizado-exercicio-01

---
+++ ex01/index.js
@@ -0,0 +1,8 @@
+let dog = {
+    name: "Pochita",
+    numLegs: 4
+  };
+  
+  console.log(dog); //{ name: 'Pochita', numLegs: 4 }
+  /*Neste desafio eu vou ter que passar os valores de objetos na variável
+  dog */
