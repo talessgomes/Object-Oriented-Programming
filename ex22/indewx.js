a60c9f173d481d70e664d172ec033a27577b7334
talessgomes <tales.santos.gomes@outlook.com>
2022-12-21 10:15:57 -0300

    finalizado-exercicio-22

--- ex22/indewx.js
+++ ex22/indewx.js
@@ -1,3 +1,27 @@
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function() { return "Alas, this is a flightless bird.";}
+
+
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+let bird = new Bird();
+console.log(bird.fly());
+
+
 
 /*Este exercício mostra que o javascript instância o objeto até ele encontrar
 o primeiro, assim ele para. Como a primeira coisa que será analisada, vai ser
