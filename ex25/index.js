df377b450c3511b8700c2da9724d71f231e1e4db
talessgomes <tales.santos.gomes@outlook.com>
2022-12-21 11:47:59 -0300

    finalizado-exercicio-25

---
+++ ex25/index.js
@@ -0,0 +1,7 @@
+(function () {
+    console.log("A cozy nest is ready");
+})();
+
+
+/*O método immediately invoked function expression, faz com que a função
+seja invocada sem que ela tenha algum nome, assim, ela fica no modo anônimo */
